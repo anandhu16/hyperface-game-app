0fd6c5385b2f773e6bf8fd415225b55e63da9fc4
Anandhu Unnikrishnan <anandhu16>
2023-09-04 02:43:14 +0530

    leaderboard-half

--- src/Components/LeaderBoard/LeaderBoard.jsx
+++ src/Components/LeaderBoard/LeaderBoard.jsx
@@ -1,212 +1,58 @@
 import "./leaderboard.css";
 
-const LeardBoard = () => {
-  const dados = [
+const LeaderBoard = () => {
+  const userData = [
     {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
+      userName: "anandhu1",
+      win: 5,
+      loss: 3,
     },
     {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
+      userName: "anandhu2",
+      win: 7,
+      loss: 3,
     },
     {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
+      userName: "anandhu3",
+      win: 3,
+      loss: 3,
     },
     {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
-    },
-    {
-      id: 1,
-      name: "Laura",
-      image: "https://cdn-icons-png.flaticon.com/512/186/186037.png",
-      level: 16,
-      xp: 100,
-      coins: 500,
-      love: 6,
-      beacons: 2,
-      resources: 70,
+      userName: "anandhu4",
+      win: 2,
+      loss: 3,
     },
   ];
+
+  const india = [1, 2, 4, 5];
+
+  // const userData = JSON.parse(localStorage.getItem("userDataList"));
+  userData.sort((a, b) => b.win - a.win);
+  console.log(userData);
   return (
-    <div className="container">
-      <div className="top-container">hai</div>
-      <div className="lower-container"></div>
+    <div className="leaderboard-container">
+      <div className="leaderboard-top3-container">
+        <div className="leaderboard-podium">
+          {userData[1] && <div className="leaderboard-second">f</div>}
+          {userData[0] && <div className="leaderboard-first">s</div>}
+          {userData[2] && <div className="leaderboard-third">s</div>}
+        </div>
+      </div>
+      <div className="leaderboard-lower-container">
+        {userData.map((user, index) => (
+          <div>
+            {index > 2 && (
+              <div>
+                <span>{index + 1}</span>
+                <span>{user.userName}</span>
+                <span>{user.win}</span>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default LeardBoard;
+export default LeaderBoard;
